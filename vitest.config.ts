6fdb205d6426359801cd3dcbf9b3063ec53d1322
samurun <fadlan.8291@gmail.com>
2026-01-16 16:51:54 +0700

    feat: Integrate Codecov for coverage reporting and enable lcov output in Vitest.

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,7 +4,7 @@ export default defineConfig({
     test: {
         coverage: {
             provider: 'v8',
-            reporter: ['text', 'json', 'html', 'json-summary'],
+            reporter: ['text', 'json', 'html', 'json-summary', 'lcov'],
         },
     },
 })
